5ca019659ec7d771c364666a9b50645246863521
Rajan <krajan92946@gmail.com>
2024-08-28 15:31:56 +0530

    Java DSA With apna college

---
+++ Selection sort/selection.java
@@ -0,0 +1,37 @@
+
+
+public class selection {
+    public static void PrintArray(int arr[])
+    {
+        for(int i=0; i<arr.length; ++i)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int []arr={100,52,35,482,46,4};
+        System.out.println("Before Sorting using selection sort");
+        for(int i=0; i<arr.length; ++i)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+        for(int i=0; i<arr.length-1; ++i)
+        {
+            int smallest=i;
+           for(int j=i+1; j<arr.length; ++j)
+           {
+               if(arr[smallest]>arr[j])
+               {
+                   smallest=j;
+               }
+           }
+           int temp=arr[smallest];
+              arr[smallest]=arr[i];
+              arr[i]=temp;
+        }
+       System.out.println("\nAfter Using selection sort");
+        PrintArray(arr);
+
+    }
+}
---
+++ Sorting Techniques/Bubble Sort/bubble.java
@@ -0,0 +1,28 @@
+public class bubble {
+    public static void main(String args[]){
+        int []arr={45,86,4,5,6,75,7};
+        System.out.println("Before Sorting Bubble sort");
+        for(int i=0; i<arr.length; ++i)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        for(int i=0; i<arr.length-1; ++i)
+        {
+            for(int j=0; j<arr.length-i-1; ++j)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("\nAfter Sorting");
+        for(int i=0; i<arr.length; ++i)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
